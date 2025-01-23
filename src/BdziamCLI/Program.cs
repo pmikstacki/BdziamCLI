2b99f432192e151103bf1123f5eb6cd4d3ce5f62
Piotr Mikstacki <piotr.mikstacki@moto-profil.pl>
2025-01-23 16:01:40 +0100

    Markdown

--- src/BdziamCLI/Program.cs
+++ src/BdziamCLI/Program.cs
@@ -1,33 +1,17 @@
 ﻿using Pidgin;
 using Pidgin.Expression;
-
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;  // we'll be parsing strings - sequences of characters. For other applications (eg parsing binary file formats) TToken may be some other type (eg byte).
 Console.WriteLine($"Hello {TextFormat.Underline.Apply(TextFormat.Bold.Apply("World"))}!");
 
 Console.WriteLine($"And now {ControlCode.Sequence('m', BasicColor.Blue.Foreground)}COO{ControlCode.Sequence('m', BasicColor.Cyan.Foreground)}OL{ControlCode.Sequence('m', BasicColor.Magenta.Foreground)}ORS{ControlCode.Sequence('m', BasicColor.Default.Foreground)}"); 
-//var markdown = "Markdown!!! **THIS IS BOLD** \n *THIS IS ITALIC* \n  **Bold *Italic Bold*** \n ~~STRIKETROUGH~~ \n __Underline__ \n ~~__STRIKETROUGH UNDERLINE~~__ ";
-//Console.WriteLine(AnsiFormatter.FromMarkdown());
-
-var tokenParser = Parser.String("**");
-var anyParser = Parser.AnyCharExcept("**").ManyString();
-var startBold = ControlCode.Sequence(FinalSeqToken.SGR, TextFormat.Bold.Init);
-var endBold = ControlCode.Sequence(FinalSeqToken.SGR, TextFormat.Bold.Reset);
-
-var blockParser = Parser.Try(tokenParser
-            .Then(anyParser.Before(tokenParser))    
-            .Select(content => startBold + content + endBold));
-
-var resultingParser = Parser.Map((leftAny, block, rightAny) => leftAny + block + rightAny,
-    anyParser,
-    blockParser,
-    anyParser
-).ManyString();
+var markdown = "Markdown!!! **THIS IS BOLD** \n *THIS IS ITALIC* \n  **Bold *Italic Bold*** \n ~~STRIKETROUGH~~ \n __Underline__ \n ~~__STRIKETROUGH UNDERLINE~~__ ";
 
 
-var result = resultingParser.Parse("TESTS **TEST BOLD** -TEST AGAIN- *DIFFERENT TEST*");
-if(!result.Success)
-{
-    Console.WriteLine(result.Error.RenderErrorMessage());
-}
-else{
-    Console.WriteLine(string.Join("", result.Value));
-}
+AnsiFormatter.FromMarkdown("**this**");
+
+//AnsiFormatter.FromMarkdown(markdown);
+
+//var anyToken = OneOf(AnsiFormatter.markdownFormatRules.Select(x => String(x.Token))).ManyString();
+//var anyNonMarkdownParser = Any.Until(anyToken);
+//Console.WriteLine(anyNonMarkdownParser.Parse(markdown).GetValueOrDefault());
